df962af8d858948a8154497b5674546dcf6adefd
Adebayo Akanbi <akanbieniola@gmail.com>
2025-07-04 08:59:01 -0700

    Add files via upload

--- src/scenes/Navbar/Navbar.tsx
+++ src/scenes/Navbar/Navbar.tsx
@@ -2,55 +2,77 @@ import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
+import type { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/UseMediaQuery";
+import { div } from "framer-motion/client";
+import ActionButton from "@/shared/ActionButton";
 
 type Props = {
-  selectedPage: string;
-  setSelectedPage: (value: string) => void;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
 
-  return <nav>
-    <div 
-    className ={`${flexBetween} fixed top-0 z-30 w-full py-6` }
-    >
-      <div className ={`${flexBetween} mx-auto w-5/6`}>
-      <div className ={`${flexBetween} mx-full gap-16`}>
-        {/* LEFT SIDE */}
-        <img alt="logo" src={Logo} />
+  return (
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* LEFT SIDE */}
+            <img alt="logo" src={Logo} />
 
-        {/*RIGHT SIDE*/}
-        <div className ={`${flexBetween} w-full`}>
-          {/* INNER LEFT SIDE */}
-          <div className ={`${flexBetween} gap-8 text-sm`}>
-            <Link 
-              page = "Home"
-              selectedPage={selectedPage}
-              setSelectedPage= {setSelectedPage}/>
-            <Link 
-              page = "Benefits"
-              selectedPage={selectedPage}
-              setSelectedPage= {setSelectedPage}/>
-            <Link 
-              page = "Our Classes"
-              selectedPage={selectedPage}
-              setSelectedPage= {setSelectedPage}/>
-            <Link 
-              page = "Contact Us"
-              selectedPage={selectedPage}
-              setSelectedPage= {setSelectedPage}/>
+            {/*RIGHT SIDE*/}
+            {isAboveMediumScreens ? (
+              <div className="flex items-center justify-between w-full">
+                {/* INNER LEFT SIDE */}
+                <div className="flex items-center gap-8">
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+
+                {/* INNER RIGHT SIDE */}
+                <div className="flex items-center gap-8">
+                  <p>Sign In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a member
+                  </ActionButton>
+                </div>
+              </div>
+            ) : (
+              <button
+                className="rounded-full bg-Secondary-500 p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3Icon className="h-6 w-6 text-white"></Bars3Icon>
+              </button>
+            )}
           </div>
-          {/* INNER RIGHT SIDE */}
-          <div className ={`${flexBetween} gap-8`}>
-            <p>Sign In</p>
-            <button>Become a member</button>
-          </div>
-         </div>
+        </div>
       </div>
-      </div>
-    </div>
-  </nav>
+    </nav>
+  );
 };
 
 export default Navbar;
